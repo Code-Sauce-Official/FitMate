f48b2e51bad30017b45e7d9c4caba76726aad8cf
Rohit Sharma <65807152+Rohit-2602@users.noreply.github.com>
2021-06-14 23:02:41 +0530

    Update Partner Request (#39)

--- Code/app/src/main/java/com/acash/fitmate/fragments/UserInfoFragment.kt
+++ Code/app/src/main/java/com/acash/fitmate/fragments/UserInfoFragment.kt
@@ -2,11 +2,11 @@ package com.acash.fitmate.fragments
 
 import android.app.ProgressDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.acash.fitmate.R
 import com.acash.fitmate.activities.MainActivity
 import com.acash.fitmate.activities.createProgressDialog
@@ -38,8 +38,8 @@ class UserInfoFragment : Fragment() {
     private lateinit var progressDialog: ProgressDialog
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_info, container, false)
@@ -61,50 +61,67 @@ class UserInfoFragment : Fragment() {
         tvState.text = form.state
 
         database.collection("users").document(form.uid)
-            .get()
-            .addOnSuccessListener {
-                progressDialog.dismiss()
-                if (it.exists()) {
-                    val user = it.toObject(User::class.java)
-                    user?.downloadUrlDp.let { url ->
-                        if (url != "") {
-                            Glide.with(this).load(url)
-                                .placeholder(R.drawable.defaultavatar)
-                                .error(R.drawable.defaultavatar).into(userImgView)
+                .get()
+                .addOnSuccessListener {
+                    progressDialog.dismiss()
+                    if (it.exists()) {
+                        val user = it.toObject(User::class.java)
+                        user?.downloadUrlDp.let { url ->
+                            if (url != "") {
+                                Glide.with(this).load(url)
+                                        .placeholder(R.drawable.defaultavatar)
+                                        .error(R.drawable.defaultavatar).into(userImgView)
+                            }
                         }
                     }
                 }
-            }
-            .addOnFailureListener {
-                Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
-            }
+                .addOnFailureListener {
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                }
 
         sendRequestBtn.setOnClickListener {
             (activity as MainActivity).currentUserInfo?.let { currUser ->
                 progressDialog = requireContext().createProgressDialog("Sending Request...", false)
                 progressDialog.show()
                 val request = Request(
-                    auth.uid.toString(),
-                    currUser.name,
-                    currUser.yearOfBirth,
-                    currUser.gender,
-                    currUser.state,
-                    currUser.downloadUrlDp
+                        auth.uid.toString(),
+                        currUser.name,
+                        currUser.yearOfBirth,
+                        currUser.gender,
+                        currUser.state,
+                        currUser.downloadUrlDp
                 )
 
-                realtimeDb.reference.child("requests/${form.uid}/${auth.uid.toString()}")
-                    .setValue(request)
-                    .addOnSuccessListener {
-                        progressDialog.dismiss()
-                        Toast.makeText(
-                            requireContext(),
-                            "Request sent successfully.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                    .addOnFailureListener {
-                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
-                    }
+                val inbox = realtimeDb.reference.child("inbox/${currUser.uid}/${form.uid}")
+
+                inbox.get()
+                        .addOnSuccessListener { inboxDataSnapshot ->
+                            if (inboxDataSnapshot.exists()) {
+                                progressDialog.dismiss()
+                                Toast.makeText(requireContext(), "This User is Already Your Partner", Toast.LENGTH_SHORT).show()
+                            } else {
+                                realtimeDb.reference.child("requests/${form.uid}/${auth.uid.toString()}")
+                                        .get()
+                                        .addOnSuccessListener { dataSnapshot ->
+                                            if (!dataSnapshot.exists()) {
+                                                realtimeDb.reference.child("requests/${form.uid}/${auth.uid.toString()}").setValue(request)
+                                                progressDialog.dismiss()
+                                                Toast.makeText(requireContext(), "Partner Request Sent", Toast.LENGTH_SHORT).show()
+                                                return@addOnSuccessListener
+                                            }
+                                            progressDialog.dismiss()
+                                            Toast.makeText(requireContext(), "Partner Request Already Sent", Toast.LENGTH_SHORT).show()
+                                        }
+                                        .addOnFailureListener { exception ->
+                                            progressDialog.dismiss()
+                                            Toast.makeText(requireContext(), exception.message, Toast.LENGTH_SHORT).show()
+                                        }
+                            }
+                        }
+                        .addOnFailureListener { exception ->
+                            progressDialog.dismiss()
+                            Toast.makeText(requireContext(), exception.message, Toast.LENGTH_SHORT).show()
+                        }
             }
         }
 
